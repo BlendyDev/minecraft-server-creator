0cfafd683ae64c44117c47b35514658516d593f2
Blendy <63923395+BlendyDev@users.noreply.github.com>
2022-07-13 18:32:03 +0200

    Initial commit

---
+++ createServer.py
@@ -0,0 +1,61 @@
+import requests
+import platform
+import download
+import json
+versions = ["1.19", "1.18.2", "1.17.1", "1.16.5", "1.15.2", "1.14.4", "1.13.2", "1.12.2", "1.11.2", "1.10.2", "1.9.4", "1.8.8"]
+files = ["vanilla", "paper","fabric", "forge"]
+softwares = ["Vanilla", "Paper", "Fabric", "Forge"]
+
+def setup():
+    #Startup script + EULA
+    RAM = input("Enter Amount of ram in GB: ")
+    RAM = int(RAM)
+    if platform.system() == "Windows":
+        sb = open("../start.bat", "w")
+    else:
+        sb = open("../start.sh", "w")
+        sb.write("#!/bin/sh\n")
+    sb.write(f"java -Xmx{RAM}G -jar Server.jar --nogui")
+    sb.close()
+    eula = open("../eula.txt", "w")
+    eula.write("eula=true")
+    eula.close()
+    
+def downloadSoftware(index):
+    #Software chooser
+    f = open("json/" + files[index] + ".json")
+    data = json.load(f)
+    print("")
+    print("*"*20)
+    print(softwares[index] + " versions:\n")
+    i = 1
+    for version in data["Versions"]:
+        print(f"{i} # {version}")
+        i = i+1
+    print("*"*20)
+    verid = int(input("Enter id: "))
+    
+    while (not (verid > 0 and verid <= len(data["Versions"]))):
+        print("Invalid Option")
+        verid = int(input("Enter id: "))
+    v = data[versions[verid-1]]
+    setup()
+    download.download(v, "../Server.jar")
+    f.close()   
+    print("Server successfully setup!")
+    
+def menu():
+    #Start UI
+    print("*"*20)
+    i = 1
+    for sw in softwares:
+        print(f"{i} # {sw}")
+        i = i+1
+    print("*"*20)
+    chosenSw = int(input("Please entesr id: "))
+    while (not (chosenSw > 0 and chosenSw <= len(softwares))): 
+        print("Invalid Option")
+        chosenSw = int(input("Please enter id: "))
+    downloadSoftware(chosenSw-1)
+            
+menu()

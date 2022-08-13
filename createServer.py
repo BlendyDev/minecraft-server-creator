766fc6c654848272de1d014b9cb9d3984a15c7d7
Blendy <63923395+BlendyDev@users.noreply.github.com>
2022-08-13 17:32:24 +0200

    Update createServer.py
    Fixed typo in syntax (i should start testing pr's)

--- createServer.py
+++ createServer.py
@@ -12,7 +12,7 @@ import platform
 import json
 from os.path import exists
 os.mkdir("Server")
-os.chidr("Server")
+os.chdir("Server")
 versions = ["1.19", "1.18.2", "1.17.1", "1.16.5", "1.15.2", "1.14.4", "1.13.2", "1.12.2", "1.11.2", "1.10.2", "1.9.4", "1.8.8"]
 files = ["vanilla", "paper","fabric", "forge"]
 link = {
